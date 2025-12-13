cd4815245dc357df6e3ca5a86456f399b734817b
0-shang <capelasucks@gmail.com>
2025-12-13 19:43:28 +0000

    ss

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'sales-master-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json'
+];
+
+// 安装 Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 拦截网络请求 (简单的网络优先策略，保证数据最新)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// 更新 Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
